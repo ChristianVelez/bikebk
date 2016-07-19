5180cf69442365cec1bf4e7e79ebf43267d23a19
Christian Velez <christian.velez1@gmail.com>
2016-07-19 16:14:19 -0400

    5.4 styled login form

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
 
   get 'orders/index'
 
+  get 'users/new'
+
   #get 'products/:id' => 'products#show'#
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
