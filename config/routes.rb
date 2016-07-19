08c3ed13786777a9742d15a250083bbc15025837
Christian Velez <christian.velez1@gmail.com>
2016-07-19 11:22:00 -0400

    styled sessions and other views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  
+  resources :users
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
   
@@ -11,6 +14,8 @@ Rails.application.routes.draw do
   root 'static_pages#landing_page'
 
   post 'static_pages/thank_you'
+
+  get 'orders/index'
 
   #get 'products/:id' => 'products#show'#
 
